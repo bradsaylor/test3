cd79a95b3faf4e91dd4f5f91594117d22780a49d
Brad Saylor <bradsaylor@yahoo.com>
2017-04-27 23:35:45 -0400

    Client asked me to change a comment
    the client asked for the comment to be added. I made that
    change for them.
    
    *Client asked for comment change
    *I don't see this affecting anything
    
    Closes-Bug: 129161

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: rbs
+ *
+ * Created on April 17, 2017, 9:35 PM
+ */
+
+
+#include <xc.h>
+#include <stdint.h>
+#include "Int_lcd_spi.h"
+#include "Int_SPI_pic.h"
+//blah blah
+
+int main(void) {
+SPI_init();
+while(1);
+return 0;
+}
